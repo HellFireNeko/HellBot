ed972e246612e0ce90178d93589bb80e3956afa5
HellFireNeko <33993757+HellFireNeko@users.noreply.github.com>
2023-11-02 11:35:04 +0100

    Adding some new checks

---
+++ HellBotLib/Checks/SlashRequireNsfw.cs
@@ -0,0 +1,13 @@
+﻿using DSharpPlus.SlashCommands;
+
+namespace HellBotLib.Checks;
+
+/// <summary>
+/// Defines that usage of this command is restricted to NSFW channels.
+/// </summary>
+[AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+public sealed class SlashRequireNsfwAttribute : SlashCheckBaseAttribute
+{
+    public override Task<bool> ExecuteChecksAsync(InteractionContext ctx)
+        => Task.FromResult(ctx.Channel.Guild == null || ctx.Channel.IsNSFW);
+}
